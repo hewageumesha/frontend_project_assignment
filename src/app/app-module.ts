b534c5335bc1fc5fa137b6aa01727581fa01280c
dil <n.u.m.hewage@gmail.com>
2026-02-09 22:18:34 +0530

    Initial commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,18 +1,22 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     App
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay()),
   ],
