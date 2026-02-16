a83df09319cd502870026db326ba0e5ef64ab821
dil <n.u.m.hewage@gmail.com>
2026-02-16 21:00:06 +0530

    Environment Variable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,14 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontProjectAssignment');
+  is_production = environment.PRODUCTION;
+  ngOnInit(): void {
+    if(this.is_production) {
+      console.log('Running in production mode');
+    } else {
+      console.log('Running in development mode');
+    }
+  }
 }
